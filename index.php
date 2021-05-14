027ceb115f56455f46238e71f5f5d3cb195ee529
kaustubh29-gif <51872311+kaustubh29-gif@users.noreply.github.com>
2021-05-14 11:00:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home(Hostel)</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="img/logo.png" type="img/icon" rel="icon">
+    <script type="text/javascript" src="jquery/jquery.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $('#btn').click(function(){
+          
+
+        });
+      });
+    </script>
+   
+
+
+</head>
+<body>
+    <div id="full">
+    
+        <div style="background-image: url('img/b1.jpg'); width:auto; height:auto; ">
+        
+        <div id="header">
+            <div id="logo">
+            <img src="img\logo.png" width="50" height="50" style="float:left;"></img> 
+            </div>
+            <div id="nav">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="help.php">Help</a></li>
+                    <li><a href="facility.php">Facility</a></li>
+                    <li><a href="r1.php">Registration</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="banner">
+        
+        </div>
+    </div>
+    </div>
+    <div id="f1">
+    </div>
+    </div>
+    <div id="headl">
+    <marquee>CHANDIGARH UNIVERSITY HOSTELS.....!!!!</marquee>
+    </div>
+
+    <div id="para">
+    <br>
+      <font face="garamond" size="4" color="black">
+      <p>
+          <h1 align="center">Hostels – Home Away from Home</h1>
+          <br>
+          <br>
+Chandigarh University Campus has separate hostels for boys and girls with all modern facilities and attached mess. The hostel mess serves fresh and hygienic food. You will be provided with facilities like internet, music room, common room with TV & cable facility, room for indoor games and a reading room with a collection of national/international newspapers, magazines, etc. The hostels houses have a fully equipped gym and separate courts for Volley Ball, Basket Ball, and Badminton. The concerned staff looks after the university assets and ensures that the hostel remains responsive all the time.
+
+          <br>
+          <br>
+          <br>
+          The Hostel provides an ideal place for a disciplined, healthy and congenial atmosphere
+for peaceful stay and study. To the residents, it is a home away from home, hence they
+consider themselves to belong to one family and treat one another in friendly & courteous
+manner. The hostel is governed by a set of rules & regulations contained in this Manual, to be
+          strictly followed by the hostel residents.</p>
+          <br>
+          
+              <br>
+
+        <div class="g1">
+        	<div class="slides">
+        		<input type="radio" name="r" id="r1" checked>
+        		<input type="radio" name="r" id="r2">
+        		<input type="radio" name="r" id="r3">
+        		<input type="radio" name="r" id="r4">
+
+        		<div class="slide s1"><img src="img/1s.jpg" ></div>
+        	<div class="slide"><img src="img/2s.jpg" alt=""></div>
+        	<div class="slide"><img src="img/3s.jpg" alt=""></div>
+        	<div class="slide"><img src="img/4s.jpg" alt=""></div>
+        	</div>
+        </div>  
+
+        <div class="navigation">
+        	<label for="r1" class="bar"></label>
+        	<label for="r2" class="bar"></label>
+        	<label for="r3" class="bar"></label>
+        	<label for="r4" class="bar"></label>
+        </div>    
+    
+    </div>
+
+    <br>
+  <br>        
+<div id="container">
+
+  <U><h3>Location</h3></U>
+<font face="garamond" size="3" color="black">
+  <br> 
+<p> Hostel for graduates and post graduates,<br>
+Chandigarh University,
+Gharuan ,Mohali,
+Punjab-140413,
+   
+<br>Ph :- 9753980665</p>
+<div id="map">
+<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3428.194699561434!2d76.57367037976636!3d30.76911449101882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ffb140bd63e07%3A0x68591e334d17a988!2sChandigarh%20University!5e0!3m2!1sen!2sin!4v1614848732969!5m2!1sen!2sin" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+ </font>
+
+</div>
+</div>
+</div>
+</body>
+</html>
